a682daf50d23a2ccd3d68b64108d91d2cc18c312
alexandr <sashkaageev@gmail.com>
2020-12-08 05:03:43 +0300

    added Duplex Linked List

---
+++ DuplexLinkedList/Model/Item.cs
@@ -0,0 +1,16 @@
+namespace DuplexLinkedList.Model
+{
+  public class Item<T>
+  {
+    public T Data { get; set; }
+    public Item<T> Previous { get; set; }
+    public Item<T> Next { get; set; }
+    
+    public Item(T data)
+    {
+      Data = data;
+    }
+
+    public override string ToString() => Data.ToString();
+  }
+}
---
+++ DuplexLinkedList/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using DuplexLinkedList.Model;
+
+namespace DuplexLinkedList
+{
+  public static class Program
+  {
+    private static void Main(string[] args)
+    {
+      Console.WriteLine("*** Duplex Linked list ***");
+      
+      var duplex = new DuplexLinkedList<int>();
+      duplex.Add(1);
+      duplex.Add(2);
+      duplex.Add(3);
+      duplex.Add(4);
+      duplex.Add(5);
+      
+
+      foreach (var item in duplex)
+      {
+        Console.WriteLine(item);
+      }
+    }
+  }
+}
